f5ef4c2bd85a9dc0daf63322f0643e1773ca6ada
Adnan <adnan214@gmail.com>
2019-01-10 07:48:30 -0500

    Added basic  driverSign() and test for it

--- contracts/LeaseAgreement.sol
+++ contracts/LeaseAgreement.sol
@@ -23,6 +23,24 @@ contract LeaseAgreement {
     uint public start_timestamp = 0;
     uint public end_timestamp = 0;
 
+    bool is_driver_signed = false;
+    bool is_owner_signed = false;
+
+    bool is_started = false;
+    bool is_ended = false;
+
+    uint public driver_deposit_required = 3;
+    uint public driver_deposit_amount = 0;
+
+    uint public owner_deposit_required = 1;
+    uint public owner_deposit_amount = 0;
+
+    uint public driver_balance = 0;
+
+    event DraftCreated(address the_car, address the_driver, uint start_timestamp, uint end_timestamp);
+    event DriverSigned(address the_car, address the_driver, uint deposit_amount);
+    event DriverBalanceUpdated(address the_car, address the_driver, uint new_balance);
+    
     constructor (
         address _car, 
         address _driver, 
@@ -35,6 +53,24 @@ contract LeaseAgreement {
         the_driver = _driver;
         start_timestamp = _start_timestamp;
         end_timestamp = _end_timestamp;
+
+        emit DraftCreated(the_car, the_driver, start_timestamp, end_timestamp);
+    }
+
+    function driverSign() public payable {
+        require(msg.sender == the_driver, "Only driver can sign agreement!");
+        require(is_driver_signed == false, "Agreement has already been signed by driver");
+        require(msg.value >= driver_deposit_required, "Insufficient deposit amount!");
+
+        driver_deposit_amount = driver_deposit_required;
+        is_driver_signed = true;
+        emit DriverSigned(the_car, the_driver, driver_deposit_amount);
+
+        // add any extra $ to the driver's balance
+        if (msg.value > driver_deposit_required) {
+            driver_balance = msg.value - driver_deposit_required;
+            emit DriverBalanceUpdated(the_car, the_driver, driver_balance);
+        }
     }
 
 }
---
+++ test/test_agreement_signing.js
@@ -0,0 +1,82 @@
+
+
+var assert = require('assert');
+const Web3 = require('web3');
+
+var LeasableCarArtifact = artifacts.require("LeasableCar");
+var LeaseAgreementArtifact = artifacts.require("LeaseAgreement");
+
+// This returns a promise even because its async. 
+// This code works fine when called directly rather than in a function
+// needs to do a promise chain to resolve it
+// async function create_agreement(the_car, start_timestamp, end_timestamp, driver_uid) {
+//     var tx = await the_car.
+//     requestContractDraft(start_timestamp, end_timestamp, 
+//         {from: driver_uid});
+//     var agreement_uid = tx.logs[0].args.contractAddress;
+//     const agreement = await LeaseAgreementArtifact.at(agreement_uid);
+//     return agreement;
+// }
+
+contract('TestRequestContract', async function(accounts) {
+
+    var car1;
+    var car1_uid;
+    // var car1_agreement;
+
+    var car_owner_uid = accounts[0];
+    var driver_uid = accounts[1];
+    var some_other_account = accounts[2]
+
+    var g = 4712388;
+    var gp = 100000000000;
+
+    before(async function() {
+        // create a car from acct1
+        car1 = await LeasableCarArtifact
+            .new('VIN1231', '2019', 'Audi', 'S4', 'Blue', 99, 
+            {from: car_owner_uid, gas: g, gasPrice: gp}
+        );
+        car1_uid = car1.address
+        // console.log("test car uid: " + car1_uid);
+    });
+
+    it("Checking driverSign with exact deposit amount...", async function() {
+
+        var start_timestamp = 1543838400;
+        // December 9, 2018 11:59:59 AM
+        var end_timestamp = 1544356799;
+
+        // const car1_agreement = create_agreement(
+        //     car1, start_timestamp, end_timestamp, driver_uid);      
+
+        var tx = await car1.
+        requestContractDraft(start_timestamp, end_timestamp, 
+            {from: driver_uid});
+    
+        var agreement_uid = tx.logs[0].args.contractAddress;
+        const car1_agreement = await LeaseAgreementArtifact.at(agreement_uid);
+                    
+        deposit_in = 3;
+        var tx = await car1_agreement.
+            driverSign(
+                {from: driver_uid,
+                    value: 3,
+                });
+
+        assert.equal(tx.logs.length, 1, "New LeaseAgreement creation should only have 1 event!");
+        assert.ok(tx.logs[0].args, "No args in tx!");
+        assert.equal(tx.logs[0].event, "DriverSigned", "No DriverSigned event emitted!");
+        assert.equal(tx.logs[0].args.the_car, car1_uid, "DriverSigned car_uid is bad!");
+        assert.equal(tx.logs[0].args.the_driver, driver_uid, "DriverSigned driver_uid is bad!");
+        assert.equal(tx.logs[0].args.deposit_amount.toNumber(), 3, "DriverSigned deposit amount is bad!");
+
+        var driver_deposit_amount = await car1_agreement.driver_deposit_amount.call();
+        assert.equal(driver_deposit_amount, deposit_in, "Driver depoist amount is not right!");
+
+        var driver_balance_amount = await car1_agreement.driver_balance.call();
+        assert.equal(driver_balance_amount, 0, "Driver balance amount is not right!");
+
+    });
+
+});
