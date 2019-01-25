442818985168421f9ed74a3949e5b8d27d81f48e
Adnan <adnan214@gmail.com>
2019-01-25 12:03:16 -0500

    Added Ropsten network config

--- truffle-config.js
+++ truffle-config.js
@@ -55,7 +55,7 @@ module.exports = {
       provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/${infuraKey}`),
       network_id: 4,          // Rinkeby's id
       gas: 5500000,        
-      skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
+      // skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Another network with more advanced options...
@@ -70,14 +70,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    ropsten: {
+      provider: () => new HDWallet(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    },
 
     // Useful for private networks
     // private: {
