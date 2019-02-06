37ee57f46561fd54447ee94eeb58a57cbf4b970e
Adnan <adnan214@gmail.com>
2019-02-05 19:59:28 -0500

    Fixed reference to toWei()

--- migrations/2_deploy_test_cars.js
+++ migrations/2_deploy_test_cars.js
@@ -1,13 +1,13 @@
 var LeasableCar = artifacts.require("./LeasableCar.sol")
 
 const ether = 10**18
-var web3 = require("web3");
+var web3_utils = require("web3-utils");
 
 module.exports = function(deployer, _network, _accounts) {
     deployer.deploy(LeasableCar, 
         'VIN1231', '2019', 'Audi', 'S4', 'Blue', 
-        web3.utils.toWei(1+''));
+        web3_utils.toWei(1+''));
     deployer.deploy(LeasableCar, 
         'VIN4567', '2017', 'Merc', 'S500', 'Black', 
-        web3.utils.toWei(1.5+''));
+        web3_utils.toWei(1.5+''));
 };
